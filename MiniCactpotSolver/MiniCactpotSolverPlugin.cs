53faee02960667a32f032bf594bde5cd250bb9df
Raymond Lynch <daemitus@gmail.com>
2021-04-16 16:08:09 -0400

    Don't deal with viewports, remove ImGui

--- MiniCactpotSolver/MiniCactpotSolverPlugin.cs
+++ MiniCactpotSolver/MiniCactpotSolverPlugin.cs
@@ -1,8 +1,6 @@
 ﻿using Dalamud.Plugin;
-using ImGuiNET;
 using System;
 using System.Linq;
-using System.Numerics;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using System.Threading.Tasks;
@@ -18,19 +16,18 @@ namespace MiniCactpotSolver
 
         private const int TotalNumbers = PerfectCactpot.TotalNumbers;
         private const int TotalLanes = PerfectCactpot.TotalLanes;
+        private int[] GameState = new int[TotalNumbers];
 
         public void Initialize(DalamudPluginInterface pluginInterface)
         {
             Interface = pluginInterface ?? throw new ArgumentNullException(nameof(pluginInterface), "DalamudPluginInterface cannot be null");
 
-            Interface.UiBuilder.OnBuildUi += UiBuilder_OnBuildUi_Overlay;
             Interface.Framework.OnUpdateEvent += GameUpdater;
         }
 
         public void Dispose()
         {
             Interface.Framework.OnUpdateEvent -= GameUpdater;
-            Interface.UiBuilder.OnBuildUi -= UiBuilder_OnBuildUi_Overlay;
         }
 
         #region GameLogic
@@ -61,55 +58,46 @@ namespace MiniCactpotSolver
             if (Interface.ClientState.TerritoryType != 144)  // Golden Saucer
                 return;
 
-            bool ready = false;
-            var addon = Interface.Framework.Gui.GetUiObjectByName("LotteryDaily", 1);
-            if (addon != IntPtr.Zero)
+            var ready = false;
+            var isVisible = false;
+            AddonLotteryDaily* addon = null;
+            var addonPtr = Interface.Framework.Gui.GetUiObjectByName("LotteryDaily", 1);
+
+            if (addonPtr != IntPtr.Zero)
             {
-                var uiAddon = MiniCactpotGameData.Addon = (AddonLotteryDaily*)addon;
-                var rootNode = uiAddon->AtkUnitBase.RootNode;
+                addon = (AddonLotteryDaily*)addonPtr;
+                var rootNode = addon->AtkUnitBase.RootNode;
                 if (rootNode != null)
                 {
-                    MiniCactpotGameData.X = rootNode->X;
-                    MiniCactpotGameData.Y = rootNode->Y;
-                    MiniCactpotGameData.Width = (ushort)(rootNode->Width * rootNode->ScaleX);
-                    MiniCactpotGameData.Height = (ushort)(rootNode->Height * rootNode->ScaleY);
-                    MiniCactpotGameData.IsVisible = uiAddon->AtkUnitBase.IsVisible;
+                    isVisible = addon->AtkUnitBase.IsVisible;
                     ready = true;
                 }
             }
 
             if (!ready)
-            {
-                MiniCactpotGameData.Addon = null;
-                MiniCactpotGameData.X = 0;
-                MiniCactpotGameData.Y = 0;
-                MiniCactpotGameData.Width = 0;
-                MiniCactpotGameData.Height = 0;
-                MiniCactpotGameData.IsVisible = false;
                 for (int i = 0; i < TotalNumbers; i++)
-                    MiniCactpotGameData.GameState[i] = 0;
-            }
+                    GameState[i] = 0;
 
-            if (!MiniCactpotGameData.IsVisible)
+            if (!isVisible)
                 return;
 
-            var gameState = GetGameState();
-            if (!Enumerable.SequenceEqual(gameState, MiniCactpotGameData.GameState))
+            var gameState = GetGameState(addon);
+            if (!Enumerable.SequenceEqual(gameState, GameState))
             {
-                MiniCactpotGameData.GameState = gameState;
+                GameState = gameState;
 
                 if (!gameState.Contains(0))
                 {
                     // Perform this check for when the entire board is revealed, no unknowns/zeroes
                     for (var i = 0; i < TotalNumbers; i++)
-                        ToggleGameNode(i, false);
+                        ToggleGameNode(addon, i, false);
                     for (var i = 0; i < TotalLanes; i++)
-                        ToggleLaneNode(i, false);
+                        ToggleLaneNode(addon, i, false);
                 }
                 else
                 {
                     for (var i = 0; i < TotalNumbers; i++)
-                        ToggleGameNode(i, false);  // Reset the number colors
+                        ToggleGameNode(addon, i, false);  // Reset the number colors
 
                     var solution = PerfectCactpot.Solve(gameState);
 
@@ -129,44 +117,33 @@ namespace MiniCactpotSolver
                         };
 
                         for (var i = 0; i < TotalNumbers; i++)
-                            ToggleGameNode(i, false);  // Reset the number colors
+                            ToggleGameNode(addon, i, false);  // Reset the number colors
 
                         for (var i = 0; i < TotalLanes; i++)
-                            ToggleLaneNode(i, solution[i]);
+                            ToggleLaneNode(addon, i, solution[i]);
                     }
                     else
                     {
                         for (var i = 0; i < TotalNumbers; i++)
-                            ToggleGameNode(i, solution[i]);
+                            ToggleGameNode(addon, i, solution[i]);
                     }
                 }
             }
         }
 
-        internal static unsafe class MiniCactpotGameData
+        private unsafe int[] GetGameState(AddonLotteryDaily* addon)
         {
-            public static AddonLotteryDaily* Addon = null;
-            public static float X = 0;
-            public static float Y = 0;
-            public static ushort Width = 0;
-            public static ushort Height = 0;
-            public static bool IsVisible = false;
-            public static int[] GameState = new int[TotalNumbers];
+            return Enumerable.Range(0, TotalNumbers).Select(i => addon->GameNumbers[i]).ToArray();
         }
 
-        private unsafe int[] GetGameState()
+        private unsafe void ToggleGameNode(AddonLotteryDaily* addon, int i, bool enable)
         {
-            return Enumerable.Range(0, TotalNumbers).Select(i => MiniCactpotGameData.Addon->GameNumbers[i]).ToArray();
+            ToggleNode(addon->GameBoard[i]->AtkComponentButton.AtkComponentBase.OwnerNode, enable);
         }
 
-        private unsafe void ToggleGameNode(int i, bool enable)
+        private unsafe void ToggleLaneNode(AddonLotteryDaily* addon, int i, bool enable)
         {
-            ToggleNode(MiniCactpotGameData.Addon->GameBoard[i]->AtkComponentButton.AtkComponentBase.OwnerNode, enable);
-        }
-
-        private unsafe void ToggleLaneNode(int i, bool enable)
-        {
-            ToggleNode(MiniCactpotGameData.Addon->LaneSelector[i]->AtkComponentBase.OwnerNode, enable);
+            ToggleNode(addon->LaneSelector[i]->AtkComponentBase.OwnerNode, enable);
         }
 
         private unsafe void ToggleNode(AtkComponentNode* node, bool enable)
@@ -186,53 +163,5 @@ namespace MiniCactpotSolver
         }
 
         #endregion GameLogic
-
-        #region ImGui
-
-        private void UiBuilder_OnBuildUi_Overlay()
-        {
-            if (!MiniCactpotGameData.IsVisible)
-                return;
-
-            var x = MiniCactpotGameData.X;
-            var y = MiniCactpotGameData.Y;
-            var w = MiniCactpotGameData.Width;
-            var h = MiniCactpotGameData.Height;
-            ImGui.SetNextWindowPos(new Vector2(x + w / 2 - 3, y + h - 35), ImGuiCond.Always);
-            ImGui.SetNextWindowSize(new Vector2(w / 2, 30), ImGuiCond.Always);
-            ImGui.SetNextWindowBgAlpha(0.0f);
-
-            ImGui.PushStyleColor(ImGuiCol.ResizeGrip, 0);  // Hide the resize window grip
-            ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, new Vector2(0, 0));
-
-            bool alwaysTrue = true;
-            ImGui.Begin("FFXIV Cactpot Solver", ref alwaysTrue,
-                ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoCollapse |
-                ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar |
-                ImGuiWindowFlags.NoTitleBar);
-
-            var poweredText = $"Powered by PerfectCactpot ";
-            var tooltipText = "(?)";
-            var textSize = ImGui.CalcTextSize(poweredText + tooltipText);
-            ImGui.SetCursorPosX(ImGui.GetCursorPosX() + ImGui.GetColumnWidth() - textSize.X - ImGui.GetScrollX() - 2 * ImGui.GetStyle().ItemSpacing.X);  // right aligned
-            ImGui.Text(poweredText);
-            ImGui.SameLine();
-            ImGui.TextDisabled(tooltipText);
-            if (ImGui.IsItemHovered())
-            {
-                ImGui.BeginTooltip();
-                ImGui.Text("https://super-aardvark.github.io/yuryu/");
-                ImGui.Text("Original by /u/yuryu");
-                ImGui.Text("Improved recommendations by /u/aureolux and /u/super_aardvark");
-                ImGui.EndTooltip();
-            }
-
-            ImGui.End();
-
-            ImGui.PopStyleVar();
-            ImGui.PopStyleColor();
-        }
-
-        #endregion ImGui
     }
 }
