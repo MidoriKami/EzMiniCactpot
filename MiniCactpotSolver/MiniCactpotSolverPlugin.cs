e4ab8c3eca16c0d65da21cafb7db0cb72f4b43f0
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-06 19:34:34 -0700

    Use Service Object

--- MiniCactpotSolver/MiniCactpotSolverPlugin.cs
+++ MiniCactpotSolver/MiniCactpotSolverPlugin.cs
@@ -4,11 +4,6 @@ using System.Linq;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using System.Threading.Tasks;
-using Dalamud.Game;
-using Dalamud.Logging;
-using Dalamud.Game.ClientState;
-using Dalamud.Game.Gui;
-using Dalamud.IoC;
 using Dalamud.Plugin.Services;
 
 namespace MiniCactpotSolver;
@@ -17,65 +12,46 @@ public sealed class MiniCactpotPlugin : IDalamudPlugin
 {
     private const int TotalNumbers = PerfectCactpot.TotalNumbers;
     private const int TotalLanes = PerfectCactpot.TotalLanes;
-    private int[] GameState = new int[TotalNumbers];
+    private int[] gameState = new int[TotalNumbers];
 
-    internal DalamudPluginInterface Interface { get; init; }
-    internal IChatGui ChatGui { get; init; }
-    internal IClientState ClientState { get; init; }
-    internal IFramework Framework { get; init; }
-    internal IGameGui GameGui { get; init; }
-    internal IPluginLog PluginLog { get; init; }
-
-    public MiniCactpotPlugin(
-        DalamudPluginInterface pluginInterface,
-        IChatGui chatGui,
-        IClientState clientState,
-        IFramework framework,
-        IGameGui gameGui,
-        IPluginLog pluginLog)
+    public MiniCactpotPlugin(DalamudPluginInterface pluginInterface)
     {
-        Interface = pluginInterface ?? throw new ArgumentNullException(nameof(pluginInterface), "DalamudPluginInterface cannot be null");
+        pluginInterface.Create<Service>();
 
-        ChatGui = chatGui;
-        ClientState = clientState;
-        Framework = framework;
-        GameGui = gameGui;
-        PluginLog = pluginLog;
-
-        Framework.Update += FrameworkLotteryPoll;
+        Service.Framework.Update += FrameworkLotteryPoll;
     }
 
     public void Dispose()
     {
-        Framework.Update -= FrameworkLotteryPoll;
+        Service.Framework.Update -= FrameworkLotteryPoll;
     }
 
     #region GameLogic
 
-    private readonly PerfectCactpot PerfectCactpot = new();
-    private Task GameTask;
+    private readonly PerfectCactpot perfectCactpot = new();
+    private Task gameTask;
 
     private void FrameworkLotteryPoll(IFramework framework)
     {
         try
         {
-            if (ClientState.TerritoryType != 144)  // Golden Saucer
+            if (Service.ClientState.TerritoryType != 144)  // Golden Saucer
                 return;
 
-            var addonPtr = GameGui.GetAddonByName("LotteryDaily", 1);
+            var addonPtr = Service.GameGui.GetAddonByName("LotteryDaily");
             if (addonPtr == IntPtr.Zero)
                 return;
 
-            if (GameTask == null || GameTask.IsCompleted || GameTask.IsFaulted || GameTask.IsCanceled)
+            if (gameTask == null || gameTask.IsCompleted || gameTask.IsFaulted || gameTask.IsCanceled)
             {
-                GameTask = Task.Run(() => GameUpdater(addonPtr));
+                gameTask = Task.Run(() => GameUpdater(addonPtr));
             }
         }
         catch (OperationCanceledException) { }
         catch (Exception ex)
         {
-            PluginLog.Error(ex, "Updater has crashed");
-            ChatGui.PrintError($"ezMiniCactpot has encountered a critical error");
+            Service.PluginLog.Error(ex, "Updater has crashed");
+            Service.ChatGui.PrintError($"ezMiniCactpot has encountered a critical error");
         }
     }
 
@@ -96,19 +72,19 @@ public sealed class MiniCactpotPlugin : IDalamudPlugin
         {
             for (int i = 0; i < TotalNumbers; i++)
             {
-                GameState[i] = 0;
+                this.gameState[i] = 0;
             }
         }
 
         if (!isVisible)
             return;
 
-        var gameState = GetGameState(addon);
-        if (!Enumerable.SequenceEqual(gameState, GameState))
+        var localGameState = GetGameState(addon);
+        if (!localGameState.SequenceEqual(this.gameState))
         {
-            GameState = gameState;
+            this.gameState = localGameState;
 
-            if (!gameState.Contains(0))
+            if (!localGameState.Contains(0))
             {
                 // Perform this check for when the entire board is revealed, no unknowns/zeroes
                 for (var i = 0; i < TotalNumbers; i++)
@@ -121,12 +97,12 @@ public sealed class MiniCactpotPlugin : IDalamudPlugin
                 for (var i = 0; i < TotalNumbers; i++)
                     ToggleGameNode(addon, i, false);  // Reset the number colors
 
-                var solution = PerfectCactpot.Solve(gameState);
+                var solution = perfectCactpot.Solve(localGameState);
 
                 if (solution.Length == 8)
                 {
                     // The PerfectCactbot lane array is formatted differently than the UI when it gives lane solutions.
-                    solution = new bool[]
+                    solution = new[]
                     {
                         solution[6],  // major diagonal
                         solution[3],  // left column
---
+++ MiniCactpotSolver/Service.cs
@@ -0,0 +1,15 @@
+﻿using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace MiniCactpotSolver;
+
+internal class Service
+{
+    [PluginService] internal static DalamudPluginInterface Interface { get; private set; } = null!;
+    [PluginService] internal static IChatGui ChatGui { get; private set; } = null!;
+    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
+    [PluginService] internal static IFramework Framework { get; private set; } = null!;
+    [PluginService] internal static IGameGui GameGui { get; private set; } = null!;
+    [PluginService] internal static IPluginLog PluginLog { get; private set; } = null!;
+}
